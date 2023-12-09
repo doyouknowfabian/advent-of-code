5129ad5d4ec3432961de4d0ed36a7030e60d0587
Fabian Velasquez <doyouknowfabian@gmail.com>
2023-12-09 06:32:23 -0500

    2023 - day 4, part 2 - Scratchcards
    The amount of numbers matched to the winning numbers grant you a copy for each of the next amount
    of cards. The puzzle answer is the amount of total scratchcards you end up with given this new
    winning condition.

--- 2023/day4/main.rs
+++ 2023/day4/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::collections::HashMap;
 use std::convert::TryInto;
 use std::iter::FromIterator;
 
@@ -10,7 +11,7 @@ mod read_input;
 fn part_one() -> () {
 	let input = read_lines("./input.txt");
 	let mut points = 0;
-	for line in input  {
+	for line in input {
 		let winning = HashSet::<_>::from_iter(line.split(": ")
 													  .nth(1)
 													  .unwrap()
@@ -40,6 +41,43 @@ fn part_one() -> () {
 	println!("part one: {}", points);
 }
 
+fn part_two() -> () {
+	let input = read_lines("./input.txt");
+	let mut count = 0;
+	let mut counter = HashMap::new();
+	for i in 0..input.len() {
+		counter.insert(i, 1);
+	}
+	for (i, line) in input.into_iter().enumerate() {
+		count += counter.get(&i).unwrap();
+		let winning = HashSet::<_>::from_iter(line.split(": ")
+													  .nth(1)
+													  .unwrap()
+													  .split(" | ")
+													  .nth(0)
+													  .unwrap()
+													  .split_whitespace()
+													  .map(|x| x.parse::<i32>()
+													  .unwrap()));
+		let your = HashSet::<_>::from_iter(line.split(": ")
+												   .nth(1)
+												   .unwrap()
+												   .split(" | ")
+												   .nth(1)
+												   .unwrap()
+												   .split_whitespace()
+												   .map(|x| x.parse::<i32>()
+												   .unwrap()));
+		let intersection = your.intersection(&winning)
+							   .collect::<HashSet<_>>();
+		for j in i+1..i+1+intersection.len() {
+			let amt = counter.get(&j).unwrap() + counter.get(&i).unwrap();
+			counter.insert(j, amt);
+		}
+	}
+	println!("part two: {}", count);
+}
 fn main() {
 	part_one();
+	part_two();
 }
