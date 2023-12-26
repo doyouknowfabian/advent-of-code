d44ffd2181a0cfa7c6136335ef6503ff9d02aed1
Fabian Velasquez <doyouknowfabian@gmail.com>
2023-12-26 10:59:47 -0500

    2023 - day 6, part 2, Wait For It
    The puzzle changed to now ignore the whitespace that was delimiting each race. This means that the
    input is now one giant race with one overall time and one overall distance. This seemed to punish
    anyone who brute forced part 1 but the binary search solution I implemented was able to get the new
    puzzle answer of how many ways to there are to beat this new big race. The only change was to parsing the input.

--- 2023/day6/main.rs
+++ 2023/day6/main.rs
@@ -2,7 +2,7 @@ use read_input::read_lines;
 
 mod read_input;
 
-fn binary_upper(start: i32, end: i32, current: i32, total_time: i32, distance: i32) -> i32 {
+fn binary_upper(start: i64, end: i64, current: i64, total_time: i64, distance: i64) -> i64 {
 	if start >= end {
 		let calculated_distance = start * (total_time - start);
 		if calculated_distance > distance {
@@ -12,8 +12,8 @@ fn binary_upper(start: i32, end: i32, current: i32, total_time: i32, distance: i
 		}
 	} else {
 		let mid = (end - start) / 2 + start;
-		let calculated_distance: i64 = mid as i64 * (total_time - mid) as i64;
-		if calculated_distance > distance as i64 {
+		let calculated_distance = mid * (total_time - mid);
+		if calculated_distance > distance {
 			return binary_upper(mid + 1, end, mid, total_time, distance);
 		} else {
 			return binary_upper(start, mid - 1, current, total_time, distance);
@@ -21,7 +21,7 @@ fn binary_upper(start: i32, end: i32, current: i32, total_time: i32, distance: i
 	}
 }
 
-fn binary_lower(start: i32, end: i32, current: i32, total_time: i32, distance: i32) -> i32 {
+fn binary_lower(start: i64, end: i64, current: i64, total_time: i64, distance: i64) -> i64 {
 	if start >= end {
 		let calculated_distance = start * (total_time - start);
 		if calculated_distance > distance {
@@ -31,8 +31,8 @@ fn binary_lower(start: i32, end: i32, current: i32, total_time: i32, distance: i
 		}
 	} else {
 		let mid = (end - start) / 2 + start;
-		let calculated_distance: i64 = mid as i64 * (total_time - mid) as i64;
-		if calculated_distance > distance as i64 {
+		let calculated_distance = mid * (total_time - mid);
+		if calculated_distance > distance {
 			return binary_lower(start, mid - 1, mid, total_time, distance);
 		} else {
 			return binary_lower(mid + 1, end, current, total_time, distance);
@@ -42,30 +42,55 @@ fn binary_lower(start: i32, end: i32, current: i32, total_time: i32, distance: i
 
 fn part_one() -> () {
 	let input = read_lines("./input.txt");
-	let mut times: Vec<i32> = input[0].split(":")
+	let times: Vec<i64> = input[0].split(":")
 									  .nth(1)
 									  .unwrap()
 									  .split_whitespace()
-									  .map(|x| x.parse::<i32>().unwrap())
+									  .map(|x| x.parse::<i64>().unwrap())
 									  .collect();
-	let mut distances: Vec<i32> = input[1].split(":")
+	let distances: Vec<i64> = input[1].split(":")
 										  .nth(1)
 										  .unwrap()
 										  .split_whitespace()
-										  .map(|x| x.parse::<i32>().unwrap())
+										  .map(|x| x.parse::<i64>().unwrap())
 										  .collect();
 	let races = times.iter().zip(distances.iter());
 	let mut margin: i64 = 1;
 	for race in races {
 		let (time, distance) = race;
-		let midpoint = (time/2);
+		let midpoint = time/2;
 		let upper_bound = binary_upper(midpoint + 1, *time, 0, *time, *distance);
 		let lower_bound = binary_lower(0, midpoint, 0, *time, *distance);
-		margin *= ((upper_bound - lower_bound) + 1) as i64;
+		margin *= (upper_bound - lower_bound) + 1;
 	}
 	println!("part one: {}", margin);
 }
 
+fn part_two() -> () {
+	let input = read_lines("./input.txt");
+	let times: Vec<&str> = input[0].split(":")
+									  .nth(1)
+									  .unwrap()
+									  .split_whitespace()
+									  .collect();
+	let distances: Vec<&str> = input[1].split(":")
+										  .nth(1)
+										  .unwrap()
+										  .split_whitespace()
+										  .collect();
+	let overall_time: i64 = times.join("")
+								 .parse::<i64>()
+								 .unwrap();
+	let overall_distance: i64 = distances.join("")
+								 .parse::<i64>()
+								 .unwrap();
+	let midpoint = overall_time/2;
+	let upper_bound = binary_upper(midpoint + 1, overall_time, 0, overall_time, overall_distance);
+	let lower_bound = binary_lower(0, midpoint, 0, overall_time, overall_distance);
+	println!("part two: {}", (upper_bound - lower_bound) + 1);
+}
+
 fn main() {
 	part_one();
+	part_two();
 }
